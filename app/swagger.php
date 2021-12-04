d588ff75675d75964b46e765a6cd6ccdc48d1889
RimlTempest <riml.slime@gmail.com>
2021-12-05 04:20:45 +0900

    add: swagger自動生成、タグの設定

---
+++ app/swagger.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * @OA\Info(
+ *     version="0.0.1",
+ *     title="LaraNext-server",
+ *     description="LaraNextのAPIサーバ",
+ * )
+ * 
+ * @OA\Server(
+ *     description="OpenApi host",
+ *     url="http://localhost:3000"
+ * )
+ */
